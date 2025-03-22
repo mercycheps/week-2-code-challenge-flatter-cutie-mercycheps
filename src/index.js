24cf31e3662bc22f5f8478a20b587ed1b2f0f0f3
Mercy <maritimmercy8@gmail.com>
2025-03-22 09:03:06 +0300

    added a new fetch

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
+function displaycharacterInfo(e) {
+    e.preventDefault()
+    fetch(`http://localhost:3000/characters/${e.target.id}`)
+        .then((response) => response.json())
+        .then((characterJson) => {
+            // show character name, image, votes
+            const characterNameElem = document.getElementById('name')
+            const characterImageElem = document.getElementById('image')
+            const characterVoteCountElem = document.getElementById('vote-count')
+            characterNameElem.innerText = characterJson.name
+            characterImageElem.src = characterJson.image
+            characterVoteCountElem.innerText = characterJson.votes
+        });
+
+}
+
 
 
 document.addEventListener("DOMContentLoaded", function () {
@@ -9,6 +25,8 @@ document.addEventListener("DOMContentLoaded", function () {
             console.log(data)
             data.forEach(element => {
                 const character = document.createElement('span')
+                character.id = element.id
+                character.addEventListener("click", displaycharacterInfo)
                 character.innerText = element.name
                 charaterBar.appendChild(character)
             });
