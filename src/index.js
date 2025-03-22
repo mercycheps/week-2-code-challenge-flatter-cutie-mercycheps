25bc1a8344a5e919a2e43c185713d9f0a856a8ea
Mercy <maritimmercy8@gmail.com>
2025-03-22 08:22:21 +0300

    added appendchild

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-// Your code here
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const charaterBar = document.getElementById('character-bar')
+
+    fetch(`http://localhost:3000/characters`)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data)
+            data.forEach(element => {
+                const character = document.createElement('span')
+                character.innerText = element.name
+                charaterBar.appendChild(character)
+            });
+        });
+});
